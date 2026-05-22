5b6a951f9457bbf51ec3a03f42309e73eefa37b0
Alisson Estrada <alissonestrada881@gmail.com>
2026-05-22 00:47:07 -0500

    Config variables entorno

--- config.py
+++ config.py
@@ -5,9 +5,11 @@ Contiene las constantes que se usan en toda la aplicacion:
 la URL de la API y la clave secreta para sesiones/flash.
 """
 
-API_BASE_URL = "http://localhost:5034"
-VERIFY_SSL = False
-SECRET_KEY = "clave-secreta-flask-frontend-2024"
+import os
+
+API_BASE_URL = os.getenv("API_BASE_URL", "http://localhost:5034")
+VERIFY_SSL = os.getenv("VERIFY_SSL", "False").lower() in ("true", "1", "t")
+SECRET_KEY = os.getenv("SECRET_KEY", "clave-secreta-flask-frontend-2024")
 
 if not VERIFY_SSL:
     import urllib3
